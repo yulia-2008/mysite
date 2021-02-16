8e73b1e11d169e887d7bde6a6338af04c4bd2b7e
yulia <juliana.ny2008@gmail.com>
2021-02-16 14:30:06 -0500

    updating test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      
       <div>
         <p> TEST TEST </p>
       </div>
