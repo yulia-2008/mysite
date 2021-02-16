3bc2f78539872027cb0bbc794857081450208c5c
yulia <juliana.ny2008@gmail.com>
2021-02-16 15:57:41 -0500

    rmoving slash from routes

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ class App extends React.Component {
         <HashRouter>
          <Switch>           
               <NavBar/>
-              <Route exact path = '/' component={About} />  
-              <Route exact path = '/projects' component={Projects} />
-              <Route exact path = '/blog' component={Blog} />
-              <Route exact path = '/resume' component={Resume} />
-              <Route exact path = '/contact' component={Contact} />
+              <Route exact path = '' component={About} />  
+              <Route exact path = 'projects' component={Projects} />
+              <Route exact path = 'blog' component={Blog} />
+              <Route exact path = 'resume' component={Resume} />
+              <Route exact path = 'contact' component={Contact} />
                          
         </Switch> 
         </HashRouter>  
