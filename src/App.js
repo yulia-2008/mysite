77277c5b2d408fe8fdc06aab3df204ca7b764622
yulia <juliana.ny2008@gmail.com>
2021-02-16 15:22:38 -0500

    working on router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
+// import About from './About';
+// import Blog from './Blog';
+// import Resume from './Resume';
+// import Projects from './Projects';
+// import Contact from './Contact';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
+
+class App extends React.Component {
+
+
+ render() {
   return (
-    <div>
-      
-      <div>
-        <p> TEST TEST TEST test</p>
-      </div>
+    <div id="flex">
+         
+         <Router>           
+              <NavBar/>
+              <Route exact path = '/projects' render = {() => "project"} />
+              <Route exact path = '/blog' render = {() => "blog"} />
+              <Route exact path = '/resume' render = {() => "resume"} />
+              <Route exact path = '/contact' render = {() => "contact"} />
+              <Route exact path = '/' render = {() => "about"} />            
+        </Router> 
+        
+    
     </div>
-   
   );
+ }
 }
 
 export default App;
+
