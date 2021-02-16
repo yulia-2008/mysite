d3e06f213a6057d2df1264fe28a27b52bf08a134
yulia <juliana.ny2008@gmail.com>
2021-02-16 15:37:20 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 // import Resume from './Resume';
 // import Projects from './Projects';
 // import Contact from './Contact';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter, Route, Switch} from 'react-router-dom';
 
 
 class App extends React.Component {
@@ -15,16 +15,16 @@ class App extends React.Component {
  render() {
   return (
     <div id="flex">
-         
-         <Router>           
+        <HashRouter>
+         <Switch>           
               {/* <NavBar/> */}
               <Route exact path = '/projects' render = {() => "project"} />
               <Route exact path = '/blog' render = {() => "blog"} />
               <Route exact path = '/resume' render = {() => "resume"} />
               <Route exact path = '/contact' render = {() => "contact"} />
               <Route exact path = '/' render = {() => "about"} />            
-        </Router> 
-        
+        </Switch> 
+        </HashRouter>  
     
     </div>
   );
