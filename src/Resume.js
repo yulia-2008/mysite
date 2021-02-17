36789732ddda3a470cfac16be6789d8cc444dd19
yulia <juliana.ny2008@gmail.com>
2021-02-16 19:02:03 -0500

    working on resume

--- src/Resume.js
+++ src/Resume.js
@@ -11,6 +11,21 @@ class Resume extends Component {
                 <a href="https://github.com/yulia-2008"> GitHub</a>
                 <a href="https://www.linkedin.com/in/yuliya-yurkevich-b82a591b8/"> LinkedIn</a>
                 <a href="https://juliana-ny2008.medium.com/" target="_blank"> Blog</a>    
+                <p>Experienced in Ruby on Rails and JavaScript based programming. 
+                    With a Bachelor’s degree in Law and over five years
+                    of customer service and hospitality experience,
+                    I bring strong skills in team work,
+                    collaboration and working independently.
+                    Results-oriented with a positive attitude. 
+                    Looking forward to applying my software engineering skills
+                    to help your business grow. </p>
+                <h3>Technical Skills</h3>
+​                <p>Ruby, Ruby on Rails, JavaScript, React.js, 
+                    React Router, React Native, Material UI, 
+                    Cloudinary Storage, HTML, CSS, SQlite, PostgreSQL, 
+                    GitHub, API, Heroku Hosting.</p>
+
+
             </div>
         );
     }
