9ea25f82f56de05192265879751db5ff5a0b1920
yulia <juliana.ny2008@gmail.com>
2021-02-20 14:52:49 -0500

    css

--- src/Contact.js
+++ src/Contact.js
@@ -6,11 +6,12 @@ class Contact extends Component {
     render() {
         return (
             <div id="main-container">  
-                <div className="center"> 
-                    <br></br>  <br></br> <br></br> <br></br>  <br></br>     
-                    <h1>Contact</h1>          
-                    <p className="center">Feel free to contact me anytime.</p>   
-                    
+                <div id="header">
+                    <h1>Contact</h1> 
+                    <p>Feel free to contact me anytime.</p>
+                    <hr id="width-100"/>
+                </div>  
+                <div className="center">                
                     <h3>juliana.ny2008@gmail.com</h3> 
                     <h3>+1(347)3575454</h3>  
                     <a href="https://github.com/yulia-2008">
